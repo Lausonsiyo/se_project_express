b80154e7e41a2e63bede0bb4fda22fddaf117cac
LAU-AND <andreslauson@gmail.com>
2024-05-14 22:05:39 -0600

    feature routers

---
+++ controllers/clothingItems.js
@@ -0,0 +1,54 @@
+const ClothingItem = require("../models/clothingItem");
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  const { name, weather, imageUrl } = req.body;
+  // const owner = req.user._id;
+  console.log(req.user._id);
+
+  ClothingItem.create({ name, weather, imageUrl, owner })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageUrl } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem, deleteItem };
